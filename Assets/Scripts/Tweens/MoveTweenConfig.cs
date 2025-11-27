0819850a638cf84d056e484e6ff725342f384d1b
yelazzers <mazda22x@gmail.com>
2025-11-27 20:59:35 +0800

    comments

--- Assets/Scripts/Tweens/MoveTweenConfig.cs
+++ Assets/Scripts/Tweens/MoveTweenConfig.cs
@@ -1,10 +1,6 @@
 using System;
-using DG.Tweening;
-using UnityEngine;
 
 [Serializable]
 public class MoveTweenConfig : TweenConfigBase
 {
-    // [Header("Position Settings")]
-    // [SerializeField] private Vector3 endPosition;
 }
